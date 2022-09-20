08ad03b95f6014c7081ca261d1e4d2875d91e8c6
DHLong <92789701+West3124@users.noreply.github.com>
2022-09-20 08:56:58 +0700

    1st commit

---
+++ app/src/main/java/com/example/tltdd_bt5/Adapter.java
@@ -0,0 +1,42 @@
+package com.example.tltdd_bt5;
+
+import android.app.Activity;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+import android.widget.TextView;
+
+public class Adapter extends BaseAdapter {
+    private Activity activity;
+    private String[] items;
+
+    public Adapter(Activity activity, String[] items) {
+        this.activity = activity;
+        this.items = items;
+    }
+
+    @Override
+    public int getCount() {
+        return items.length;
+    }
+
+    @Override
+    public Object getItem(int i) {
+        return items[i];
+    }
+
+    @Override
+    public long getItemId(int i) {
+        return i;
+    }
+
+    @Override
+    public View getView(int i, View view, ViewGroup viewGroup) {
+        LayoutInflater inflater = activity.getLayoutInflater();
+        view = inflater.inflate(R.layout.item, null);
+        TextView tvName = (TextView) view.findViewById(R.id.tv_name);
+        tvName.setText(items[i]);
+        return view;
+    }
+}
---
+++ app/src/main/java/com/example/tltdd_bt5/MainActivity.java
@@ -0,0 +1,31 @@
+package com.example.tltdd_bt5;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+public class MainActivity extends AppCompatActivity {
+
+    private ListView listView;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        String[] items = {"Bún", "Cháo", "Phở"};
+
+        listView = (ListView) findViewById(R.id.list_view);
+        listView.setAdapter(new Adapter(this, items));
+
+        listView.setOnItemLongClickListener(new AdapterView.OnItemLongClickListener() {
+            @Override
+            public boolean onItemLongClick(AdapterView<?> adapterView, View view, int i, long l) {
+                return false;
+            }
+        });
+    }
+}
